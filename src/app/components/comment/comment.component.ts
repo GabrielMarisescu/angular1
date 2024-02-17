297e86d475616d74195c906ea0028eb9946df555
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2024-02-17 10:15:57 +0100

    Added component form & handlers

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [CommentFormComponent, CommonModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
   isExpanded = false;
+  isReplying = false;
+
   toggleExpanded() {
     this.isExpanded = !this.isExpanded;
+  }
+  toggleReplying() {
+    this.isReplying = !this.isReplying;
+
+    if (this.isReplying) {
+      this.isExpanded = true;
+    }
   }
 }
